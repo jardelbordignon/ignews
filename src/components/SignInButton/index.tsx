d47b61f71aabe09e6a8de06dcd5a6ebd7f6d4a0c
Jardel <jardel1101@gmail.com>
2021-04-15 17:55:23 -0300

    Autenticação com Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,22 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isLoggedIn = false
+  const [session] = useSession()
 
-  return isLoggedIn ?
+  console.log(session)
+
+  return session ?
     <button
       type='button'
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color='#04d361' />
-      Jardel Bordignon
+      { session.user.name }
       <FiX color='#737380' />
     </button>
 
@@ -21,6 +25,7 @@ export function SignInButton() {
     <button
       type='button'
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color='#eba417' />
       Autenticar com Github
