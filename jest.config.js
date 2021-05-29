3e815469aa54313e80295aab2a2cae8b48a9a213
Jardel <jardel1101@gmail.com>
2021-05-28 22:08:17 -0300

    Configurando Testing Library

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testIgnorePattern: ['/node_modules', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  testEnvironment: 'jsdom'
+}
